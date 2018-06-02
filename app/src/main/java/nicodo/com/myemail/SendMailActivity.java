05f69d72a978a3ca3ced57f1473e0a022c347a0a
trynx <trynx93@gmail.com>
2018-06-02 15:48:33 +0300

    Add Camera

--- app/src/main/java/nicodo/com/myemail/SendMailActivity.java
+++ app/src/main/java/nicodo/com/myemail/SendMailActivity.java
@@ -1,26 +1,58 @@
 package nicodo.com.myemail;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 public class SendMailActivity extends Activity {
+
+
+
+    private static final int REQUEST_CAPTURE_IMAGE = 100;
+    String imageFilePath;
+
+    private void openCameraIntent() { Intent pictureIntent = new Intent(
+            MediaStore.ACTION_IMAGE_CAPTURE);
+        if(pictureIntent.resolveActivity(getPackageManager()) != null){
+            //Create a file to store the image
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+                ex.getStackTrace();
+            }
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,this.getPackageName() + ".provider", photoFile);
+                pictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(pictureIntent, REQUEST_CAPTURE_IMAGE);
+            }
+        }
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -50,6 +82,66 @@ public class SendMailActivity extends Activity {
                         fromPassword, toEmailList, emailSubject, emailBody);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,
+                                    Intent data) {
+        if (requestCode == REQUEST_CAPTURE_IMAGE &&
+                resultCode == RESULT_OK) {
+            if (data != null && data.getExtras() != null) {
+//                Bitmap imageBitmap = (Bitmap) data.getExtras().get("data");
+                System.out.println("************** IN Activity RESULT **************" );
+
+                Uri uriImg = (Uri) data.getExtras().get("data");
+
+                System.out.println("**************" + uriImg.getPath() + "**************" );
+
+
+//                Uri uri = getImageUri(getApplicationContext(), imageBitmap);
+//                Uri uri = FileProvider.getUriForFile(SendMailActivity.this, BuildConfig.APPLICATION_ID + ".provider",imageBitmap);;
+
+                File imgFile = new File(getRealPathFromURI(uriImg));
+
+                System.out.println("**************" + imgFile.getPath() + "**************" );
+//                mImageView.setImageBitmap(imageBitmap); // Here get the path of the image
+            }
+        }
+    }
+
+    public Uri getImageUri(Context inContext, Bitmap inImage) {
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+        String path = MediaStore.Images.Media.insertImage(inContext.getContentResolver(), inImage, "Title", null);
+        return Uri.parse(path);
+    }
+
+    public String getRealPathFromURI(Uri uri) {
+        Cursor cursor = getContentResolver().query(uri, null, null, null, null);
+        cursor.moveToFirst();
+        int idx = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
+        return cursor.getString(idx);
+    }
+
+    public void takePicture(View view){
+        openCameraIntent();
+    }
+
+    private File createImageFile() throws IOException {
+        String timeStamp =
+                new SimpleDateFormat("yyyyMMdd_HHmmss",
+                        Locale.getDefault()).format(new Date());
+        String imageFileName = "IMG_" + timeStamp + "_";
+        File storageDir =
+                getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        imageFilePath = image.getAbsolutePath();
+        return image;
     }
 }
 
